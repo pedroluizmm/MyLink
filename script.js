80356d2540a0fa15a65cd73fbaa3fc168e76a2f4
pedroluizmm <pedrolmm123321@gmail>
2024-11-09 18:02:26 -0300

    add readme

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
 function toggleMode() {
-  //trocando o modo
+
   const html = document.documentElement
-  /*if (html.classList.contains('light')) {
-    html.classList.remove('light')
-  } else {
-    html.classList.add('light')
-  } */
+  
   html.classList.toggle('light')
 
-  //imagem
+  
   const img = document.querySelector('#profile img')
 
   if (html.classList.contains('light')) {
